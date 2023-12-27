e7cf10b6b845197057a06a963e7d5191696b6f01
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-26 23:27:52 -0300

    feat: add home manager and xmonad config

--- flake.nix
+++ flake.nix
@@ -1,25 +1,42 @@
 {
   description = "flake";
-
   inputs = {
     nixpkgs = {
       url = "github:NixOs/nixpkgs/nixos-23.11";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs = {
+        nixpkgs = {
+	  follows = "nixpkgs";
+	};
+      };
+    };
   };
-
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
   }:
   let 
     lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [
           ./configuration.nix
+        ];
+      };
+    };
+    homeConfigurations = {
+      erick = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
         ];
       };
     };
