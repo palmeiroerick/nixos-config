2ae87c909d37447d5fb8f7e53f08c8815cff9030
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-09 09:28:30 -0300

    feat(fish): add vi mode

--- home/modules/fish/config.fish
+++ home/modules/fish/config.fish
@@ -5,3 +5,15 @@ if status is-login
 end
 
 set -x PATH $PATH ~/.npm-global/bin
+
+# Vi mode
+fish_vi_key_bindings
+
+set fish_cursor_default block
+
+set fish_cursor_insert line
+
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+
+set fish_cursor_visual underscore
