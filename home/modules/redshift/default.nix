98da702e92783592f96c32f349d98327861803da
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-09 19:47:59 -0300

    change: redshift config

--- home/modules/redshift/default.nix
+++ home/modules/redshift/default.nix
@@ -7,7 +7,7 @@
         night = 1000;
       };
       dawnTime = "4:00-6:00";
-      duskTime = "18:00-20:00";
+      duskTime = "18:00-19:00";
       latitude = "-15.47";
       longitude = "-47.56";
       provider = "manual";
