1746753f85b7eaefb90c51c0a47b6c215a4b08d7
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-31 22:31:43 -0300

    change(nvim): alpha config

--- home/modules/nvim/plugins/alpha.lua
+++ home/modules/nvim/plugins/alpha.lua
@@ -15,7 +15,7 @@ dashboard.section.buttons.val = {
   dashboard.button("n", "  New File", ":ene <BAR> startinsert <cr>"),
   dashboard.button("e", "󰉋  File Explorer", ":NvimTreeOpen<cr>"),
   dashboard.button("f", "󰱼  Find File", ":Telescope find_files<cr>"),
-  dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <cr>"),
+  dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<cr>"),
   dashboard.button("q", "󰅚  Quit Neovim", ":qa<cr>"),
 }
 
