f021c0121115a2e11d76398158c656133994db00
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-31 22:32:11 -0300

    change(nvim): telescope config

--- home/modules/nvim/plugins/telescope.lua
+++ home/modules/nvim/plugins/telescope.lua
@@ -1,8 +1,23 @@
+local actions = require("telescope.actions")
+
 require("telescope").setup({
   defaults = {
+    layout_strategy = "center",
+    layout_config = {
+      center = {
+        height = 0.5,
+        preview_cutoff = 100,
+        prompt_position = "top",
+        width = 0.5,
+      },
+    },
+    sorting_strategy = "ascending",
+    prompt_prefix = " ",
     mappings = {
-      n = {
-        ["<esc>"] = require("telescope.actions").close,
+      i = {
+        ["<esc>"] = actions.close,
+        ["<Tab>"] = actions.move_selection_next,
+        ["<S-Tab>"] = actions.move_selection_previous,
       },
     },
   },
