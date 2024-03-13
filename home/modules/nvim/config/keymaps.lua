395848308f1446bc6c8c389943a922018eedefb8
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-03-13 14:06:24 -0300

    nvim: disable arrows

--- home/modules/nvim/config/keymaps.lua
+++ home/modules/nvim/config/keymaps.lua
@@ -1,5 +1,11 @@
 local keymap = vim.keymap.set
 
+-- Disable Arrows
+keymap({ "n", "v", "i", "c" }, "<Up>", "<Nop>")
+keymap({ "n", "v", "i", "c" }, "<Down>", "<Nop>")
+keymap({ "n", "v", "i", "c" }, "<Left>", "<Nop>")
+keymap({ "n", "v", "i", "c" }, "<Right>", "<Nop>")
+
 -- Tab
 keymap("n", "<Tab>", ">>")
 keymap("n", "<S-Tab>", "<<")
