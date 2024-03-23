ccd9885ece22531cc0b8e4e1baad64a588883bbd
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-03-23 14:50:51 -0300

    redshift: update config

--- home/modules/redshift/default.nix
+++ home/modules/redshift/default.nix
@@ -1,16 +1,11 @@
-{...}: {
-  services = {
-    redshift = {
-      enable = true;
-      temperature = {
-        day = 3200;
-        night = 1000;
-      };
-      dawnTime = "4:00-6:00";
-      duskTime = "18:00-19:00";
-      latitude = "-15.47";
-      longitude = "-47.56";
-      provider = "manual";
+{pkgs, ...}: {
+  home = {
+    packages = with pkgs; [
+      redshift
+    ];
+
+    file = {
+      ".config/redshift/redshift.conf".source = ./redshift.conf;
     };
   };
 }
