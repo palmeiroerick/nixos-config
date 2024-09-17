27a804f733b03a557fe80cc252abd29f8a18cfdb
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-09-17 08:57:43 -0300

    update font config

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,9 +1,27 @@
 {pkgs, ...}: {
   fonts.packages = with pkgs; [
+    noto-fonts-cjk-sans
+
     (nerdfonts.override {
       fonts = [
         "JetBrainsMono"
       ];
     })
   ];
+
+  fonts.fontconfig.defaultFonts = {
+    monospace = [
+      "JetBrainsMonoNL Nerd Font Mono"
+    ];
+
+    sansSerif = [
+      "DejaVu Sans"
+      "Noto Sans CJK JP"
+    ];
+
+    serif = [
+      "DejaVu Sans"
+      "Noto Sans CJK JP"
+    ];
+  };
 }
